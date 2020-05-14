9636b5649e77b5f82d693ac4d03b597eb805f7c5
agraoltomas <tomasangelotanyo@gmail.com>
2020-05-14 01:20:43 -0300

    add cors middleware

--- tecninfor_recibos/tecninfor_recibos/settings.py
+++ tecninfor_recibos/tecninfor_recibos/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'tecninfor_recibos',
     'recibos',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
